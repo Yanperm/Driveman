b97dda8674338ad8a27393145085b189f5b5fdcd
Mr.Partchayanan <Partchayanan.y@softubon.com>
2020-09-13 08:15:30 +0700

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
-		$this->load->model('welcome_model');
 	}
 	/* Load index page */
 	public function index()
