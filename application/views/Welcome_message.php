3e65d8287698b680fee72d67b198aa1606289852
Mr.Partchayanan <Partchayanan.y@softubon.com>
2020-09-18 19:00:49 +0700

    Update Welcome_message.php

--- application/views/Welcome_message.php
+++ application/views/Welcome_message.php
@@ -136,7 +136,7 @@
   <body>
     
 <header>
-  <nav class="navbar navbar-dark" style="background-color: #1A5276;">
+  <nav class="navbar navbar-dark" style="background-color: #183054;">
     <div class="container">
       <a class="navbar-brand" href="<?php echo base_url(); ?>">BestInvestor</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
