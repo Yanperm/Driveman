085f86f4882dab0c5730aabbabd51f3233401890
Mr.Partchayanan <Partchayanan.y@softubon.com>
2020-09-18 19:20:08 +0700

    Update Welcome_message.php

--- application/views/Welcome_message.php
+++ application/views/Welcome_message.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.72.0">
