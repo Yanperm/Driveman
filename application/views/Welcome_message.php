9f152bd89ff466ef40146c269890294d119d74f3
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2020-10-12 10:42:25 +0700

    Update Welcome_message.php

--- application/views/Welcome_message.php
+++ application/views/Welcome_message.php
@@ -199,31 +199,22 @@
           </li>        
         </ul> 
         <div class="container">
-  <div class="row">    
-    <div class="col-sm">
-     <button class="btn btn-warning btn-lg btn-block">ระบบแนะนำ</button>
+  <div class="row g-2">
+    <div class="col-6">
+      <div class="p-3 border bg-light"><button class="btn btn-warning btn-lg btn-block">ระบบแนะนำ</button></div>
     </div>
-    <div class="col-sm">
-    <button class="btn btn-success btn-lg btn-block">ฝากเงิน</button>
+    <div class="col-6">
+      <div class="p-3 border bg-light"><button class="btn btn-success btn-lg btn-block">ฝากเงิน</button></div>
     </div>
-    <div class="col-sm">
-    <button class="btn btn-primary btn-lg btn-block">ถอนเงิน</button>
-    </div>    
+    <div class="col-6">
+      <div class="p-3 border bg-light"><button class="btn btn-primary btn-lg btn-block">ถอนเงิน</button></div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light"><button class="btn btn-danger btn-lg btn-block">รายการรายได้</button></div>
+    </div>
   </div>
-  <hr/>
-  <div class="row">    
-    <div class="col-sm">
-     <button class="btn btn-danger btn-lg btn-block">ระบบแนะนำ</button>
-    </div>
-    <div class="col-sm">
-    <button class="btn btn-danger btn-lg btn-block">รายการสมาชิก</button>
-    </div>
-    <div class="col-sm">
-    <button class="btn btn-danger btn-lg btn-block">รายการรายได้</button>
-    </div>
-    
-  </div>
-</div>         
+</div>
+
         </form>        
         <?php 
             }
