7d18b0fab1f0dc769bfc1408f083df7c536146f2
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2020-10-12 11:27:12 +0700

    Update Welcome_message.php

--- application/views/Welcome_message.php
+++ application/views/Welcome_message.php
@@ -188,7 +188,7 @@
             </a>
           </li>
           <li class="nav-item active">
-            <div class="wel">
+            <div class="well">
             <b class="text-light">เครดิตคงเหลือ</b> <br/>
             <b class="text-warning"><h3>0.00</h3></b>
             </div>
